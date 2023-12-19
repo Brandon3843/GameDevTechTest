d40fb1057c50bd07cb6419e2fa6c421bd3005cf5
Brandon <33764674+Brandon3843@users.noreply.github.com>
2023-12-19 14:57:07 +0000

    initial

---
+++ Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Point class
+/// </summary>
+/// 
+public class Point
+{
+	public enum Direction
+	{
+		North, East, South, West
+	}
+
+	public Direction dir;
+	public int x;
+	public int y;
+	public int Number;
+}
---
+++ PointAnalyser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class PointAnalyser
+{
+
+
+
+    public static void test()
+    {
+        Console.WriteLine("Please input X location:");
+        string xLocation = Console.ReadLine();
+        Console.WriteLine(xLocation.ToString());
+        Console.WriteLine("Please input Y location:");
+        Console.ReadLine();
+        Console.WriteLine("Please input cone angle location:");
+    }
+
+    public static Point[] VisiblePoints(float X, float Y, float deg)
+    {
+        
+
+        return new Point[1];
+    }
+}
+
